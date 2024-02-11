dd7177b5a299ee00853380dbe3e55044d95867c2
Mahek Nizar <123421903+MahekNizar@users.noreply.github.com>
2024-02-10 20:20:30 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace TicTacToe2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.Restart = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.FinalResult = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.Restart, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.Exit, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button9, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.FinalResult, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.05263F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.13201F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.14191F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.14191F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(332, 303);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(3, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 55);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(113, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 55);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(223, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 55);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(3, 128);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 52);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(113, 128);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 52);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(223, 128);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(106, 52);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.Location = new System.Drawing.Point(3, 186);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(104, 52);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button8
+            // 
+            this.button8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button8.Location = new System.Drawing.Point(113, 186);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(104, 52);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // button9
+            // 
+            this.button9.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button9.Location = new System.Drawing.Point(223, 186);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(106, 52);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // Restart
+            // 
+            this.Restart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Restart.Location = new System.Drawing.Point(3, 244);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(104, 56);
+            this.Restart.TabIndex = 9;
+            this.Restart.Text = "New Game";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(223, 244);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(106, 56);
+            this.Exit.TabIndex = 10;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // FinalResult
+            // 
+            this.FinalResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FinalResult.AutoSize = true;
+            this.FinalResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FinalResult.Location = new System.Drawing.Point(113, 0);
+            this.FinalResult.Name = "FinalResult";
+            this.FinalResult.Size = new System.Drawing.Size(104, 64);
+            this.FinalResult.TabIndex = 11;
+            this.FinalResult.Text = "Winner :";
+            this.FinalResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 303);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(900, 900);
+            this.MinimumSize = new System.Drawing.Size(350, 350);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Label FinalResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe2
+{
+    public partial class Form1 : Form
+    {
+ 
+        // VARIABLES 
+
+        // This determines the turn that the players are on - maximum can be 9
+        int turns = 0;
+        // Random varibale can be used to determinhe CPU's moves
+        Random rand = new Random();
+        // This is the user and will be given a value of "x" or "o" based on their turn
+        String player;
+        // This is the CPU and will be given a value of "x" or "o" based on their turn
+        String CPU;
+        // List to store which buttons are filled and which are not
+        List<Button> boxes;
+        // this var is to store the random number which will decide who goes first
+        bool playerTurn;
+
+
+
+        // random variable needed to determine the first player to go
+        // random var to determine computer moves 
+
+        // computers moves are random
+
+        //what happens when a button has already been clicked
+        // can't click in it again 
+
+        // first player is x
+        // second is o
+
+        // some what to figure out who won if they are chosen at random?
+
+        // need to take turns - cant go at the same time
+
+        // register which boxes are filled and which are not so players can choose from the remaining
+
+
+        // reset button makes everything go back to empty and data srtucture is wiped
+
+
+        // max number of turns is 9
+        // if max number is reached and no one has one then display tie
+
+
+
+        // how is the computer going to move
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            // As the game starts everything is wiped to the original settings and list is remade
+            RestartGame();
+        }
+
+
+
+
+        // this class determined the CPU's moves at random based on unfilled boxes
+        private void CPUMove()
+        {
+
+        }
+
+        // checks if someone has won
+        // this class checks if there are any 3 in a row's
+        private void CheckWin()
+        {
+
+        }
+
+
+
+
+        private void BtnClick(object sender, EventArgs e)
+        {
+            // when button is clicked display players sign
+            if (!playerTurn) return;
+            // grabs the current displayed text in the button clicked and stores in button var
+            Button button = (Button)sender;
+
+            }
+            // check if it is avaliabe
+            // if not then return
+
+            // if it is avaliable then put player sign in display box 
+
+
+ 
+        }
+
+
+
+        private void StartNewGame()
+        {
+            RestartGame();
+            // going to determine first player and assign signs
+            // use random class to 
+
+            
+
+            // if player turn is a 1 set player to O and CPU to X
+            // if player turn is 0 do opposite
+            // gives 2 optins, 0 for player, 1 for CPU
+            playerTurn = rand.Next(2) == 0;
+
+            // player turn is a bool value, so if it is true then player is X if not player is O
+
+            if (playerTurn)
+            {
+                player = "X";
+                CPU = "O";
+            }
+            else
+            {
+                player = "O";
+                CPU = "X";
+            }
+        }
+
+        private void RestartGame()
+        {
+            // intialized List of buttons with clickable boxes
+            boxes = new List<Button> {button1, button2, button3, button4,
+                button5, button6, button7, button8, button9 };
+
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            // Had to create Restart separately because it is also used in the initialization of the program and i cant use the _Clikck version there.
+            // it also makes it easier to impliment in 
+            RestartGame();
+        }
+
+        // Exit button that ends the program
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            // This ends all operations and closes the code
+            this.Close();
+        }
+    }
+}
